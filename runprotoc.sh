1908c2a3434ac80dd2c16c164f73367c1cf0b69e
John Reece <jreece1567@yahoo.com>
2017-06-20 14:22:35 -0700

    Add swaggers for more services.

--- runprotoc.sh
+++ runprotoc.sh
@@ -4,5 +4,17 @@
 
 #protoc --proto_path=. --proto_path=./github.com/westfield/identity/proto --plugin=protoc-gen-code=./plugin_wrapper.sh --code_out=./output ./github.com/westfield/identity/proto/data/identity_data.proto
 
-protoc --proto_path=. --proto_path=./github.com/westfield/identity/proto --plugin=protoc-gen-code=./plugin_wrapper.sh --code_out=./output ./github.com/westfield/identity/proto/service/identity_service.proto
+protoc --proto_path=. --proto_path=./github.com/westfield/protobuf/identity --plugin=protoc-gen-code=./plugin_wrapper.sh --code_out=./output ./github.com/westfield/protobuf/identity/service/identity_service.proto 2>identity_swagger.json
+
+protoc --proto_path=. --proto_path=./github.com/westfield/protobuf/location --plugin=protoc-gen-code=./plugin_wrapper.sh --code_out=./output ./github.com/westfield/protobuf/location/service/location_service.proto 2>location_swagger.json
+
+protoc --proto_path=. --proto_path=./github.com/westfield/protobuf/messaging --plugin=protoc-gen-code=./plugin_wrapper.sh --code_out=./output ./github.com/westfield/protobuf/messaging/service/messaging_service.proto 2>messaging_swagger.json
+
+protoc --proto_path=. --proto_path=./github.com/westfield/protobuf/payment --plugin=protoc-gen-code=./plugin_wrapper.sh --code_out=./output ./github.com/westfield/protobuf/payment/service/payment_service.proto 2>payment_swagger.json
+
+protoc --proto_path=. --proto_path=./github.com/westfield/protobuf/product --plugin=protoc-gen-code=./plugin_wrapper.sh --code_out=./output ./github.com/westfield/protobuf/product/service/product_service.proto 2>product_swagger.json
+
+protoc --proto_path=. --proto_path=./github.com/westfield/protobuf/receipt --plugin=protoc-gen-code=./plugin_wrapper.sh --code_out=./output ./github.com/westfield/protobuf/receipt/service/receipt_service.proto 2>receipt_swagger.json
+
+protoc --proto_path=. --proto_path=./github.com/westfield/protobuf/databunker --plugin=protoc-gen-code=./plugin_wrapper.sh --code_out=./output ./github.com/westfield/protobuf/databunker/service/databunker_service.proto 2>databunker_swagger.json
 
--- src/main/java/com/myprotocplugin/Main.java
+++ src/main/java/com/myprotocplugin/Main.java
@@ -5,49 +5,96 @@ package com.myprotocplugin;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import com.google.api.AnnotationsProto;
+import com.google.api.HttpRule;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+import com.google.protobuf.CodedInputStream;
 import com.google.protobuf.DescriptorProtos.DescriptorProto;
 import com.google.protobuf.DescriptorProtos.EnumDescriptorProto;
 import com.google.protobuf.DescriptorProtos.EnumValueDescriptorProto;
 import com.google.protobuf.DescriptorProtos.FieldDescriptorProto;
 import com.google.protobuf.DescriptorProtos.FileDescriptorProto;
 import com.google.protobuf.DescriptorProtos.MethodDescriptorProto;
+import com.google.protobuf.DescriptorProtos.MethodOptions;
 import com.google.protobuf.DescriptorProtos.ServiceDescriptorProto;
+import com.google.protobuf.ExtensionRegistry;
 import com.google.protobuf.GeneratedMessageV3;
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.compiler.PluginProtos;
 import com.google.protobuf.compiler.PluginProtos.CodeGeneratorRequest;
 import com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse;
 import com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse.Builder;
 
 /**
- * Simple protoc plugin, produces JSON representing a .proto file.
+ * Simple protoc plugin, produces JSON representing a .proto file, as well as a
+ * Swagger-2.0-format JSON file representing <b>all</b> entities (and
+ * transitives) defined in a .proto file.
+ * <ol>
+ * <li>protoc is invoked with one or more proto files specified on the
+ * command-line.</li>
+ * <li>protoc invokes the plugin and supplies a pre-parsed AST representing the
+ * proto file(s) and their transitive dependencies to the plugin via
+ * <b>stdin</b> (in Java, <b>System.in</b>)</li>
+ * <li>protoc plugins write their output to <b>stdout</b> (in Java,
+ * <b>System.out</b>). One or more files may be written.
+ * </ol>
+ *
+ * <p>
+ * The process of developing a protoc plugin, working with the AST, and
+ * generating output is very poorly documented. Googling around helps a bit,
+ * though you have to know enough C++, Go, and Python to get much out of that.
+ * The rest is discovered via trial-and-error. This is concerning, since it is
+ * unclear whether this process will change as protoc evolves. Google should do
+ * a much better job of creating/maintaining official protoc documentation so
+ * that stable plugins can be built.
+ * </p>
  *
  * @author jreece
  *
  */
 public class Main {
 
-    // for pretty-printing the JSON we generate
+    /**
+     * for pretty-printing the JSON we generate
+     */
     private static final Gson gson = new GsonBuilder().setPrettyPrinting()
             .disableHtmlEscaping().serializeNulls().create();
 
-    // a map of package-name-->list-of-descriptors (which can be enums or
-    // messages)
+    /**
+     * a map of package-name-->list-of-descriptors (which can be enums or
+     * messages)
+     */
     private static final HashMap<String, List<GeneratedMessageV3>> entities = new HashMap<String, List<GeneratedMessageV3>>();
 
+    /**
+     * a map of package-name-->list-of-descriptors (which are enum descriptors)
+     */
     private static final HashMap<String, List<EnumDescriptorProto>> _enums = new HashMap<String, List<EnumDescriptorProto>>();
+
+    /**
+     * a map of package-name-->list-of-descriptors (which are message
+     * descriptors)
+     */
     private static final HashMap<String, List<DescriptorProto>> _messages = new HashMap<String, List<DescriptorProto>>();
+
+    /**
+     * a list-of-descriptors (which are service descriptors)
+     */
     private static final List<ServiceDescriptorProto> _services = new ArrayList<ServiceDescriptorProto>();
 
+    /**
+     * a swagger-2.0-format JSON document which is built by this program.
+     */
     private static final JsonObject swaggerDoc = new JsonObject();
 
     /**
@@ -58,15 +105,13 @@ public class Main {
      */
     private static void buildSwaggerSkeleton(final String serviceName) {
 
-        // final JsonObject swaggerDoc = new JsonObject();
-
         swaggerDoc.add("swagger", new JsonPrimitive("2.0"));
 
         final JsonObject info = new JsonObject();
-        info.add("title", new JsonPrimitive(serviceName));
+        info.add("title", new JsonPrimitive("WRS " + serviceName + " Service"));
         info.add("version", new JsonPrimitive("1.0"));
         info.add("description", new JsonPrimitive(""));
-        info.add("x-last-modified-at", new JsonPrimitive(""));
+        info.add("x-last-modified-at", new JsonPrimitive(new Date().toString()));
         final JsonObject license = new JsonObject();
         license.add("name", new JsonPrimitive("Apache 2 License"));
         license.add("url", new JsonPrimitive(
@@ -81,7 +126,7 @@ public class Main {
 
         final JsonArray tags = new JsonArray();
         final JsonObject tag = new JsonObject();
-        tag.add("name", new JsonPrimitive("IDENTITY"));
+        tag.add("name", new JsonPrimitive(serviceName.toUpperCase()));
         tags.add(tag);
         swaggerDoc.add("tags", tags);
 
@@ -123,18 +168,19 @@ public class Main {
     }
 
     /**
-     * Recursively traverse the AST and build a dictionary
+     * Recursively traverse the AST and build a dictionary of all Messages and
+     * Enums that we find.
      *
      * @param packageName
      *            the current package name from the .proto
      * @param enums
      *            the list of enums declared in the .proto
-     * @param descriptors
+     * @param messages
      *            the list of messages declared in the .proto
      */
     private static final void traverseAST(final String packageName,
             final List<EnumDescriptorProto> enums,
-            final List<DescriptorProto> descriptors) {
+            final List<DescriptorProto> messages) {
 
         // add the entries we know about
         if (entities.get(packageName) == null) {
@@ -143,8 +189,8 @@ public class Main {
         if (enums != null) {
             entities.get(packageName).addAll(enums);
         }
-        if (descriptors != null) {
-            entities.get(packageName).addAll(descriptors);
+        if (messages != null) {
+            entities.get(packageName).addAll(messages);
         }
         if (_enums.get(packageName) == null) {
             _enums.put(packageName, new ArrayList<EnumDescriptorProto>());
@@ -155,20 +201,18 @@ public class Main {
         if (_messages.get(packageName) == null) {
             _messages.put(packageName, new ArrayList<DescriptorProto>());
         }
-        if (descriptors != null) {
-            _messages.get(packageName).addAll(descriptors);
+        if (messages != null) {
+            _messages.get(packageName).addAll(messages);
         }
 
-        // loop through the descriptors, recurse to find nested enums and
-        // messages
-        for (final DescriptorProto descriptor : descriptors) {
+        // loop through the descriptors, recurse to find nested items
+        for (final DescriptorProto message : messages) {
 
-            final List<EnumDescriptorProto> _enums = descriptor
-                    .getEnumTypeList();
-            final List<DescriptorProto> _descriptors = descriptor
+            final List<EnumDescriptorProto> _enums = message.getEnumTypeList();
+            final List<DescriptorProto> _descriptors = message
                     .getNestedTypeList();
 
-            traverseAST(packageName + "." + descriptor.getName(), _enums,
+            traverseAST(packageName + "." + message.getName(), _enums,
                     _descriptors);
         }
     }
@@ -268,9 +312,9 @@ public class Main {
      * @param name
      *            the service name
      */
-    private static final void generateSwaggerJSON() {
+    private static final void generateSwaggerJSON(final String serviceName) {
 
-        // get a list of enums
+        // get the list of enums
         final Map<String, EnumDescriptorProto> enums = new HashMap<String, EnumDescriptorProto>();
         final Iterator<String> itEnums = _enums.keySet().iterator();
         while (itEnums.hasNext()) {
@@ -296,7 +340,7 @@ public class Main {
                 definitions.add(descriptor.getName(), definition);
                 definition.add("type", new JsonPrimitive("object"));
                 definition
-                        .add("additionalProperties", new JsonPrimitive(false));
+                .add("additionalProperties", new JsonPrimitive(false));
                 final JsonObject properties = new JsonObject();
                 definition.add("properties", properties);
                 final List<FieldDescriptorProto> fieldList = descriptor
@@ -334,10 +378,10 @@ public class Main {
                                         "description",
                                         new JsonPrimitive(
                                                 sb.toString()
-                                                        .substring(
-                                                                0,
-                                                                sb.toString()
-                                                                        .length() - 2)));
+                                                .substring(
+                                                        0,
+                                                        sb.toString()
+                                                        .length() - 2)));
 
                             } else {
                                 // it's a $ref to another Message
@@ -389,10 +433,10 @@ public class Main {
                                         "description",
                                         new JsonPrimitive(
                                                 sb.toString()
-                                                        .substring(
-                                                                0,
-                                                                sb.toString()
-                                                                        .length() - 2)));
+                                                .substring(
+                                                        0,
+                                                        sb.toString()
+                                                        .length() - 2)));
                             } else {
                                 // it's a $ref to another Message
                                 final String refName = "#/definitions/"
@@ -435,6 +479,7 @@ public class Main {
         for (final ServiceDescriptorProto service : _services) {
             final List<MethodDescriptorProto> methods = service.getMethodList();
             for (final MethodDescriptorProto method : methods) {
+                // extractOptions(method);
                 final String methodName = method.getName();
                 final String inParam = method.getInputType();
                 final String outParam = method.getOutputType();
@@ -443,7 +488,7 @@ public class Main {
                 final JsonObject verb = new JsonObject();
                 verbs.add("post", verb);
                 final JsonArray tags = new JsonArray();
-                tags.add(new JsonPrimitive("IDENTITY"));
+                tags.add(new JsonPrimitive(serviceName.toUpperCase()));
                 verb.add("tags", tags);
                 final JsonArray parameters = new JsonArray();
                 verb.add("parameters", parameters);
@@ -477,6 +522,80 @@ public class Main {
 
     }
 
+    private static void extractOptions(final MethodDescriptorProto method) {
+
+        final ExtensionRegistry registry = ExtensionRegistry.newInstance();
+        AnnotationsProto.registerAllExtensions(registry);
+
+        if (method.hasOptions()
+                && (method.getOptions().toString().length() > 0)) {
+            final MethodOptions options = method.getOptions();
+            System.err.println("method.getName: " + method.getName());
+            System.err
+                    .println("options.toString: [" + options.toString() + "]");
+            System.err.println("options.getSerializedSize: "
+                    + options.getSerializedSize());
+            System.err.println("options.isInitialized: "
+                    + options.isInitialized());
+            System.err
+            .println("options.hasExtension(AnnotationsProto.http) is "
+                    + options.hasExtension(AnnotationsProto.http));
+            if (options.getExtension(AnnotationsProto.http) != null) {
+
+                System.err
+                .println("options.getExtention(AnnotationsProto.http) is not null");
+            }
+            final byte[] bytes = options.toByteArray();
+            System.err.println("options.toByteArray(): " + bytesToHex(bytes));
+            System.err.println("options.toByteArray().length: " + bytes.length);
+            HttpRule rule = options.getExtension(AnnotationsProto.http);
+            System.err.println("options.getExtension(AnnotationsProto.http): "
+                    + rule.toString());
+            try {
+                rule = HttpRule.parser().parseFrom(
+                        CodedInputStream.newInstance(bytes), registry);
+                System.err
+                        .println("HttpRule.parser().parseFrom(CodedInputStream.newInstance(bytes), registry): "
+                                + rule.toString());
+                System.err.println("rule.isInitialized(): "
+                        + rule.isInitialized());
+                System.err.println("rule.getPatternCase().toString(): "
+                        + rule.getPatternCase().toString());
+                System.err.println("rule.getSelector(): " + rule.getSelector());
+                System.err.println("Unknown: "
+                        + rule.getUnknownFields().toString());
+                System.err.println("InitError: "
+                        + rule.getInitializationErrorString());
+                System.err.println("BodyBytes: "
+                        + rule.getBodyBytes().toString());
+                System.err.println("SelBytes: "
+                        + rule.getSelectorBytes().toString());
+                System.err.println("AddBind: "
+                        + rule.getAdditionalBindingsCount());
+                System.err.println("AllField: "
+                        + rule.getAllFields().toString());
+                System.err.println("Default: "
+                        + rule.getDefaultInstanceForType().toString());
+                System.err.println("Fields: " + rule.getAllFields().toString());
+            } catch (final InvalidProtocolBufferException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    public static String bytesToHex(final byte[] bytes) {
+        final char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            final int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[(j * 2) + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
     /**
      * Process a single file in the .proto file list supplied by protoc, write
      * whatever we've generated to the response-builder.
@@ -492,8 +611,13 @@ public class Main {
         // get top-level enums and messages
         final List<EnumDescriptorProto> enums = fdp.getEnumTypeList();
         final List<DescriptorProto> messages = fdp.getMessageTypeList();
+        // get the list of services
         final List<ServiceDescriptorProto> services = fdp.getServiceList();
         _services.addAll(services);
+        // if (fdp.getExtensionCount() > 0) {
+        // System.err.println("Extensions: "
+        // + fdp.getExtensionList().toString());
+        // }
 
         // set up a map to record the AST
         entities.clear();
@@ -522,26 +646,38 @@ public class Main {
      * @param responseBuilder
      *            the response-builder, to which we will write whatever we
      *            generate
+     * @return the name of the service (eg. 'identity', 'messaging', etc.)
      */
-    private static final void generate(final CodeGeneratorRequest request,
+    private static final String generate(final CodeGeneratorRequest request,
             final Builder responseBuilder) {
 
+        String protoFileName = "";
         // process each .proto file passed in the request
         for (final FileDescriptorProto fdp : request.getProtoFileList()) {
+
             // generate something from the .proto file, add that to the
             // response-builder
+            protoFileName = fdp.getName();
             System.err.println("Processing: " + fdp.getName());
             processFile(fdp, responseBuilder);
         }
-
+        protoFileName = protoFileName.substring(
+                protoFileName.lastIndexOf("/") + 1).replaceAll(
+                "_service.proto", "");
+        return protoFileName.substring(0, 1).toUpperCase()
+                + protoFileName.substring(1);
     }
 
     /**
+     * Plugin entry point. Note that command-line arguments are not used. Protoc
+     * communicates with plugins via System.in and System.out only.
+     *
      * @param args
      *            command-line arguments
      */
     public static void main(final String[] args) {
         // System.err.println("Demo protoc-plugin starting ...");
+        String serviceName = "";
 
         try {
             // get the request from System.in (stdin)
@@ -554,14 +690,19 @@ public class Main {
 
             // generate whatever we are going to generate from the request,
             // write to the response-builder
-            generate(codeGeneratorRequest, codeGeneratorResponseBuilder);
+            serviceName = generate(codeGeneratorRequest,
+                    codeGeneratorResponseBuilder);
 
             // write the response to System.out (stdout)
             codeGeneratorResponseBuilder.build().writeTo(System.out);
 
-            buildSwaggerSkeleton("Identity");
-            generateSwaggerJSON();
+            // generate the swagger-2.0 document from the proto-descriptor
+            // information we have
+            buildSwaggerSkeleton(serviceName);
+            generateSwaggerJSON(serviceName);
 
+            // write the swagger-doc to System.err - cannot use System.out since
+            // protoc uses that channel.
             System.err.println(gson.toJson(swaggerDoc));
 
         } catch (final IOException e) {
